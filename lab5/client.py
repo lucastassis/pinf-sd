3b53fe614ccf7bf2629d0fb72cab12c3bc193dad
lucastassis <lucaswfitassis@gmail.com>
2023-05-20 20:15:00 -0300

    update trab5

--- lab5/client.py
+++ lab5/client.py
@@ -4,6 +4,20 @@ import random
 import json
 import time
 import hashlib
+import uuid
+import threading as td
+import queue
+
+
+# total args
+n = len(sys.argv)
+ 
+# check args
+if (n != 2):
+    print("correct use: python client.py <broker_address>.")
+    exit()
+
+BROKER_ADDR = sys.argv[1]
 
 # From https://codereview.stackexchange.com/a/260276
 def str_bin_in_4digits(hex_string: str) -> str:
@@ -16,47 +30,55 @@ def str_bin_in_4digits(hex_string: str) -> str:
 
     return f"{int(hex_string,16):0{len(hex_string)*5-1}_b}".replace('_', '')
 
-def on_message(client, userdata, message):
-    if message.topic == 'sd/42/challenge/':
-        on_message_challenge(client, userdata, message)
-    elif message.topic == 'sd/42/result/':
-        on_message_result(client, userdata, message)
+def mine(challenge, queue, init, end):
+    for i in range(init, end):
+        if queue.empty():
+            res = hashlib.sha1(str(i).encode()).hexdigest()
+            bin_res = str_bin_in_4digits(res)
+            if bin_res[:challenge] == challenge * '0':
+                queue.put(str(i))
+                break
+        else:
+            break
 
 def on_message_challenge(client, userdata, message):
-    print("Received message on " + str(message.topic) + ": " + str(message.payload.decode("utf-8")))
     challenge_dict = json.loads(str(message.payload.decode("utf-8")))
     challenge = challenge_dict['Challenge']
     transactionID = challenge_dict['TransactionID']
-    i = 0
-    while True:
-        res = hashlib.sha1(str(i).encode()).hexdigest()
-        bin_res = str_bin_in_4digits(res)
-        if bin_res[:challenge] == challenge * '0':
-            topic = 'sd/42/solution/'
-            solution = {'ClientID' : id, 'TransactionID' : transactionID, 'Solution' : str(i)}
-            solution = json.dumps(solution).replace(' ', '')
-            client.publish(topic, solution)
-            break
-        i += 1
+    print(f'Novo desafio com ID {transactionID} and desafio {challenge} recebido!')
+
+    # search solution with multithreading
+    solutions_queue = queue.Queue()
+    size = 2 ** 32
+    threads = [td.Thread(target=mine, args=(challenge, solutions_queue, 0, int(size / 2))), td.Thread(target=mine, args=(challenge, solutions_queue, int(size / 2), int(size)))]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+    #  publish solution
+    solution = solutions_queue.get()
+    topic = 'sd/42/solution/'
+    solution = {'ClientID' : id, 'TransactionID' : transactionID, 'Solution' : solution}
+    solution = json.dumps(solution).replace(' ', '')
+    client.publish(topic, solution)
+    
     
 def on_message_result(client, userdata, message):
-    print("Received message on " + str(message.topic) + ": " + str(message.payload.decode("utf-8")))
     response = json.loads(str(message.payload.decode("utf-8")))
-    # if response['Result'] != 0:
-    #     client.loop_stop()
+    if response['Result'] != 0:
+        print(f'Parabens cliente {response["ClientID"]}, sua resposta {response["Solution"]} esta correta!')
+    elif response['Result'] == -1:
+        print(f'Desculpa cliente {response["ClientID"]}, sua solucao {response["Solution"]} esta incorreta ou o problema já foi resolvido!')
     
 # init
-id = random.randint(0, 10000000)
-print(f'Client {id}')
-broker_addr = str(sys.argv[1])
+id = uuid.uuid1().int
 client = mqtt.Client(str(id))
-client.connect(broker_addr) 
+client.connect(BROKER_ADDR) 
+print(f'Inicializando minerador {id}')
+client.subscribe('sd/42/challenge/')
+client.subscribe('sd/42/result/') 
+client.message_callback_add('sd/42/challenge/', on_message_challenge)
+client.message_callback_add('sd/42/result/', on_message_result)
 
-client.loop_start()
-topic_challenge = 'sd/42/challenge/'
-topic_result = 'sd/42/result/'
-client.subscribe(topic_challenge)
-client.subscribe(topic_result)
-client.on_message=on_message
-time.sleep(60 * 10)
-client.loop_stop()
+client.loop_forever()
--- lab5/controller.py
+++ lab5/controller.py
@@ -5,7 +5,21 @@ import pandas as pd
 import json
 import time
 import hashlib
+import uuid
 
+# total args
+n = len(sys.argv)
+
+# check args
+if (n != 2):
+    print("correct use: python controller.py <broker_address>")
+    exit()
+
+# some control vars
+BROKER_ADDR = sys.argv[1]
+class Controller:
+    transactions = pd.DataFrame()        
+       
 # From https://codereview.stackexchange.com/a/260276
 def str_bin_in_4digits(hex_string: str) -> str:
     """
@@ -18,57 +32,71 @@ def str_bin_in_4digits(hex_string: str) -> str:
     return f"{int(hex_string,16):0{len(hex_string)*5-1}_b}".replace('_', '')
 
 def on_message(client, userdata, message):
-    print("Received message on " + str(message.topic) + ": " + str(message.payload.decode("utf-8")))
     solution_dict = json.loads(str(message.payload.decode("utf-8")))
     solution = solution_dict['Solution'] 
     res = hashlib.sha1(solution.encode()).hexdigest()
     bin_res = str_bin_in_4digits(res)
-    challenge = new_challenge['Challenge']
-    if bin_res[:challenge] == challenge * '0':
-        topic = 'sd/42/result/'
-        solution = {'ClientID' : solution_dict['ClientID'], 
-                    'TransactionID' : new_challenge['TransactionID'], 
-                    'Solution' : solution,
-                    'Result' : 1}
-        solution = json.dumps(solution).replace(' ', '')
-        client.publish(topic, solution)
-        # client.loop_stop()
+    challenge = controller.transactions.iloc[-1]['Challenge']
+    winner = controller.transactions.loc[controller.transactions['TransactionID'] == solution_dict['TransactionID'], 'Winner'].item()
+    # verifica se o challenge ja foi resolvido
+    if winner == -1:
+        # resposta correta
+        if bin_res[:challenge] == challenge * '0':
+            topic = 'sd/42/result/'
+            solution = {'ClientID' : solution_dict['ClientID'], 
+                        'TransactionID' : solution_dict['TransactionID'], 
+                        'Solution' : solution,
+                        'Result' : 1}
+            solution = json.dumps(solution).replace(' ', '')
+            client.publish(topic, solution)
+            controller.transactions.loc[controller.transactions['TransactionID'] == solution_dict['TransactionID'], 'Winner'] = solution_dict['ClientID']
+            controller.transactions.loc[controller.transactions['TransactionID'] == solution_dict['TransactionID'], 'Solution'] = solution_dict['Solution']
+        # resposta incorreta
+        else:
+            topic = 'sd/42/result/'
+            solution = {'ClientID' : solution_dict['ClientID'], 
+                        'TransactionID' : solution_dict['TransactionID'], 
+                        'Solution' : solution,
+                        'Result' : 0}
+            solution = json.dumps(solution).replace(' ', '')
+            client.publish(topic, solution)
     else:
         topic = 'sd/42/result/'
         solution = {'ClientID' : solution_dict['ClientID'], 
-                    'TransactionID' : new_challenge['TransactionID'], 
+                    'TransactionID' : solution_dict['TransactionID'], 
                     'Solution' : solution,
                     'Result' : 0}
         solution = json.dumps(solution).replace(' ', '')
         client.publish(topic, solution)
 
 # init
-id = random.randint(0, 10000000)
-broker_addr = str(sys.argv[1])
+id = uuid.uuid1().int
+transactions = pd.DataFrame() # transaction table
 client = mqtt.Client(str(id))
-client.connect(broker_addr) 
+client.connect(BROKER_ADDR) 
+client.subscribe(topic='sd/42/solution/')
+client.on_message=on_message 
+controller = Controller()
 
+print('Inicializando servidor...')
+client.loop_start()
 while True:
     print('Escolha:')
     print('1 para gerar novo desafio,')
     print('2 para finalizar')
-
-    n = input('qual sua escolha: ')
+    n = input('Qual sua escolha: ')
 
     if n == '1':
-        # publish 
-        topic = 'sd/42/challenge/'
-        new_challenge = {'TransactionID' : 0, 'Challenge' : random.randint(20, 25)}
-        str_new_challenge = json.dumps(new_challenge).replace(' ', '')
-        client.publish(topic, str_new_challenge)
+        print(controller.transactions)
+        # publish new challenge
+        new_challenge = pd.DataFrame([{'TransactionID' : len(controller.transactions), 'Challenge' : random.randint(10, 20), 'Solution' : '', 'Winner' : -1}])
+        controller.transactions = pd.concat([controller.transactions, new_challenge], ignore_index=True)
+        str_new_challenge = json.dumps(controller.transactions.iloc[-1].to_dict()).replace(' ', '')
+        client.publish('sd/42/challenge/', str_new_challenge)
 
-        # subscribe
-        client.loop_start()
-        topic = 'sd/42/solution/'
-        client.subscribe(topic)
-        client.on_message=on_message 
-        time.sleep(10 * 60)
-        client.loop_stop()
+        while controller.transactions.tail(1)['Winner'].item() == -1:
+            time.sleep(1)
+        
     if n == '2':
-        print('bye')
+        print('Tchau!')
         exit()
